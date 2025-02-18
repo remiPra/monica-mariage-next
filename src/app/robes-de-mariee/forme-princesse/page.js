a8d4c77e4a4d6a0a94aba3a8475757fee3022a91
remiPra <remipradere@gmail.com>
2025-02-18 20:02:42 +0100

    ajout loader

--- src/app/robes-de-mariee/forme-princesse/page.js
+++ src/app/robes-de-mariee/forme-princesse/page.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Header from "@/app/components/Header";
 import ComponentMainPage from "@/app/components/ComponentPageMain";
+import Loader from "@/app/components/LoaderMonicaMariage";
 
 function Page() {
   return (
